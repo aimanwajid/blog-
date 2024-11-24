e275b214108feb95e6beea46a8b3a29ddec1e2d6
aimanwajid <salmawajid003@gmail.com>
2024-11-24 02:56:46 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AboutPage from "./components/about";
 import BlogPage from "./blog/blogpage";
 import Contact from "./components/contact"
 import Footer from "./components/footer";
-import {Blogs} from "./components/blog";
+
 
 export default function Home() {
   return (
